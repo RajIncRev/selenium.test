cf9ef003297d2c57d28d422f25e164064cd4c344
RajIncRev <rajkishore.behura@gmail.com>
2022-06-14 10:00:43 +0530

    initial checkin

---
+++ src/main/java/com/selenium/prj/Application.java
@@ -0,0 +1,35 @@
+package com.selenium.prj;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@SpringBootApplication
+public class Application {
+
+	public static WebDriver driver;
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		/*WebDriverManager.chromedriver().setup();
+		//C:\Apps
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		String url="https://www.google.com";
+		driver.get(url);*/
+		
+		System.setProperty("webdriver.chrome.driver","C://apps//chromedriver.exe");
+
+		//Create Chrome Browser Driver
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.google.com");
+	}
+
+}
